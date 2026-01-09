686df25d54cbe09f3f02615aa11c8c55e2476f5a
papalexios001-wq <papalexios001@gmail.com>
2026-01-09 15:02:05 +0200

    Update index.ts

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,1 +1,10 @@
+// Existing exports
+export { MetricCard } from './MetricCard';
+export { PasswordInput } from './PasswordInput';
+export { ScoreIndicator } from './ScoreIndicator';
+export { StatusBadge } from './StatusBadge';
 
+// NEW: Content block exports
+export * from './ContentBlocks';
+export { OptimizedContentRenderer } from './OptimizedContentRenderer';
+export type { OptimizationResult } from './OptimizedContentRenderer';
