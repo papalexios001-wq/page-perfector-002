08775d429e01bf58994b53a0b7617fc6518e08a0
papalexios001-wq <papalexios001@gmail.com>
2026-01-10 19:01:56 +0200

    Update App.tsx
    CRITICAL FIX: Add blog post route to App.tsx. This was the root cause of 404 errors! The dynamic route /:category/:slug will now properly render BlogPostPage component with styled components.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import BlogPostPage from "./pages/[...slug]";
 import './styles/blog-components.css';
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                      <Route path="/:category/:slug" element={<BlogPostPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
